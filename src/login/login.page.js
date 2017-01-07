722e4223e334a4bb1c4c9b1a17679a294a616d7a
Roman Mnatsakanian <romamr1111@gmail.com>
2017-01-07 21:23:19 +0200

    users from inner scope

--- src/login/login.page.js
+++ src/login/login.page.js
@@ -1,94 +1,71 @@
 import loginTemplate from './login.page.hbs';
 import {elem} from '../createElement';
-//import {users} from './mockUsers';
 import {renderUser} from "../user";
 import {renderAdmin} from "../admin";
 
-let users = [];
 
 export var curentUser = {};
 
-export function loginPage () {	
 
-	let mainPage = elem.getLink('container');
- 	mainPage.innerHTML = loginTemplate();
 
- 	let form =elem.getLink('loginForm');
- 	form.addEventListener('submit', ONSubmit);
-}
 
-function ONSubmit(event){
-	event.preventDefault();
-
-	var login = document.getElementById("login").value + '';
-	var password = document.getElementById("password").value + '';
-	var resalt = chekData(login, password);
-	if (resalt.admin){	
-		curentUser = resalt;		
-		renderAdmin(resalt);
-	} else if (resalt.name){
-		curentUser = resalt;
-		renderUser(resalt);
-	} else {
-		container.innerHTML = resalt.message;
-	}
-}
-
-function chekData(login, password){
-	var message = '';
-	var user = {};
-	for (var i = users.length - 1; i >= 0; i--) {
-		if ((users[i].name === login) && (users[i].password === password)){
-			message = 'You successfully login!!! Welcome ' + users[i].name;
-			user = users[i];
-			break;
-		} else {
-			message = 'Check the login and password!!! You are not logged in!';
-		}
-	}		
-	user.message = message;
-	return user;		
-};
-
-export function loadUser(){
-   
-
-  var xmlhttp;
-  if (window.XMLHttpRequest)
-    {// код для IE7+, Firefox, Chrome, Opera, Safari
-    xmlhttp=new XMLHttpRequest();
-    }
-  else
-    {// код для IE6, IE5
-    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-    }
-  xmlhttp.onreadystatechange=function()
-    {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200)
-      {
-      
-      users = JSON.parse(xmlhttp.responseText);
-      //console.log(users);
-      loginPage ();
-      
-      }
-    }
-  xmlhttp.open("Get","/users",true);
-  xmlhttp.send();
-  
-}
 
 
 function render(){
 	make_request('Get', '/users', function(data){
-		users = data;
-		loginPage();
+		loginPage(data);
 	});
 }
 
 
+function loginPage(users) {	
+	let mainPage = elem.getLink('container');
+ 	mainPage.innerHTML = loginTemplate();
+
+ 	let form = elem.getLink('loginForm');
+ 	form.addEventListener('submit', onSubmit);
+
+
+	function onSubmit(event){
+		event.preventDefault();
+
+		var login = document.getElementById("login").value + '';
+		var password = document.getElementById("password").value + '';
+		var resalt = ch ekData(login, password);
+		if (resalt.admin){	
+			curentUser = resalt;		
+			renderAdmin(resalt);
+		} else if (resalt.name){
+			curentUser = resalt;
+			renderUser(resalt);
+		} else {
+			container.innerHTML = resalt.message;
+		}
+	}
+
+
+
+	function chekData(login, password){
+		var message = '';
+		var user = {};
+		for (var i = users.length - 1; i >= 0; i--) {
+			if ((users[i].name === login) && (users[i].password === password)){
+				message = 'You successfully login!!! Welcome ' + users[i].name;
+				user = users[i];
+				break;
+			} else {
+				message = 'Check the login and password!!! You are not logged in!';
+			}
+		}		
+		user.message = message;
+		return user;		
+	}
+
+}
+
+
 function make_request(method, endpoint, cb){
-  const xmlhttp =new XMLHttpRequest();
+  const xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange= onreadystatechange; 
   xmlhttp.open(method, endpoint, true);
   xmlhttp.send();
